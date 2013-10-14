216ec45cb51738a237d6de154b6b500518040178
unknown <zhuhuotui@gmail.com>
2013-10-14 09:15:06 +0800

    fix minc.bat

--- windows/minc.bat
+++ windows/minc.bat
@@ -1,5 +1,5 @@
 REM the least source of microsoft c can be complite and run normally.
-%1.c < o{}
+echo o(){} > %1.c
 cl /nologo /c %1.c
-link /entry:m /subsystem:windows %1.obj
+link /entry:o /subsystem:windows %1.obj
 del %1.c %1.obj
